17e0565db2b4f770b8c75eddbb6549ec170338f5
方中勤 <714458078@qq.com>
2017-03-04 22:06:32 +0800

    nodejs静态文件服务2

--- controllers/static.js
+++ controllers/static.js
@@ -1,3 +1,26 @@
+const joinPath=require('path').join //加载JoinPath进行路径的拼接
+const fs=require('fs')
+const mime=require('mime')
+
+const publicPath=joinPath(__dirname,'../public')
+console.log(publicPath)
 module.exports=function(req,res){
-	res.end('static')
+    var path=req.params[1]
+    path=joinPath(publicPath,path)
+    fs.readFile(path,function(err,data){ //读取path路径下的文件内容
+    	if(err){
+    		if(errCode='ENOENT'){
+    			res.writeHead(404)
+    			res.end('Not Found')
+    			return
+    		}
+    		res.writeHead(500)
+    	    res.end(err.message)
+    	}
+    	var mimeType=mime.lookup(path)
+    	var charset=mime.charsets.lookup(mimeType)
+    	console.log('mimeType',mimeType,'charset',charset)
+    	res.setHeader('Content-Type',+mimeType+(charset?';charset:='+charset:''))
+    	res.end(data)
+    })
 }
--- main.js
+++ main.js
@@ -16,17 +16,24 @@ function find(arr,match){
 const rules=[
 	{path:'/',controller:controllers.home},
 	{path:'/user',controller:controllers.user},
-	{path:'/static/',controller:controllers.static}
+	{path:/\/static(\/.*)/,controller:controllers.static}
 ]
 
-var server=http.createServer(function(req,res){
-	var UrlInfo=parseURL(req.url);
+var server=http.createServer(function(req,res){   
+	var urlInfo=parseURL(req.url)
 	var rule=find(rules,function(rule){
-		return rule.path==UrlInfo.pathname
+		if(rule.path instanceof RegExp){
+			var matchResult=urlInfo.pathname.match(rule.path)  
+			if(matchResult){
+				req.params=matchResult
+			}
+			return matchResult
+		}
+		return rule.path==urlInfo.pathname
 	})
 	var controller=rule&&rule.controller||notFoundController
 	controller(req,res)
 })
 
-server.listen(3000)
+server.listen(3000)    
 console.log('server run at 127.0.0.1:3000')
